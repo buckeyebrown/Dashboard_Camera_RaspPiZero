cfbc2bf56ca378e155f5c2edcd0d86c63d37dee6
buckeyebrown <brown.4414@osu.edu>
2017-04-02 23:13:54 -0400

    night time check

--- light_sensor.py
+++ light_sensor.py
@@ -20,8 +20,8 @@ while (count < var):
     #   0x02    Nominal Integration Time = 402ms
     bus.write_byte_data(0x29, 0x01 | 0x80, 0x02)
 
-    # Sleep for 10 seconds
-    time.sleep(10)
+    # Sleep for a second
+    time.sleep(1)
 
     #Read data back from 0x0C with command register, 0x80, 2 bytes
     # ch0 LSB, ch0 MSB
@@ -34,15 +34,30 @@ while (count < var):
     infrared_data = bus.read_i2c_block_data(0x29, 0x0E | 0x80, 2)
 
     #Convert the data
-    channel0 = full_spectrum_data[1] * 256 + full_spectrum_data[0]
-    channel1 = infrared_data[1] * 256 + infrared_data[0]
+    full_spectrum_val = full_spectrum_data[1] * 256 + full_spectrum_data[0]
+    infrared_val = infrared_data[1] * 256 + infrared_data[0]
+    visible_spectrum_val = full_spectrum_val - infrared_val
 
     #Print data to screen
     print "Iteration: %d." %(count + 1)
-    print "Full Spectrum Light (IR + Visibile) : %d lux" %channel0
-    print "Infrared Value : %d lux" %channel1
-    print "Visibile Spectrum Light : %d lux" %(channel0 - channel1)
+    print "Full Spectrum Light (IR + Visibile) : %d lux" %full_spectrum_val
+    print "Infrared Value : %d lux" %infrared_val
+    print "Visibile Spectrum Light : %d lux" %visible_spectrum_val
+    isItNight = isItNightTime(visible_spectrum_val)
     print
+
+    #Sleep for 9 seconds before next iteration
+    sleep(9)
     count = count + 1
 
 
+def isItNightTime(visible_spectrum_val):
+    nightTime = False
+    minDayTimeLux = 250
+    if (visible_spectrum_val < minDayTimeLux):
+        nightTime = True
+        print "It is Night Time! Activating Camera Night Mode."
+    else:
+        print "It is Day Time! Activating Camera Day Mode."
+
+    return nightTime
