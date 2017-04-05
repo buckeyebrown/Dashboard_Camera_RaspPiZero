d55e4889f98db64f2ebe030cb9179d3ce4d0e551
buckeyebrown <brown.4414@osu.edu>
2017-04-04 22:03:39 -0400

    refactoring, combining methods

---
+++ cameraWithLightSensorAndLED.py
@@ -0,0 +1,179 @@
+import picamera
+import time
+import os
+import errno
+import sys
+import RPi.GPIO as GPIO
+import smbus
+
+#Start the Raspberry Pi Camera
+def startRaspCamera():
+    turnOffIRLED()
+    with picamera.start_preview() as camera:
+        camera.start_preview()
+        directory_path = getDirectoryPath()
+        checkIfDirectoryExistsOrCreate(directory_path)
+        filename = createFileName(directory_path)
+        startRecording(filename, directory_path, camera)
+
+    return
+
+#Get the directory path for the videos being saved
+def getDirectoryPath():
+    filelocation = "/home/pi/recorded_videos/"
+    foldername = time.strftime("%Y%m%d") + "_videos/"
+    directory_path = filelocation + foldername
+
+    return directory_path
+
+def checkIfDirectoryExistsOrCreate(directory_path):
+    if not os.path.exists(directory_path):
+        try:
+            os.makedirs(directory_path)
+        except OSError as error:
+            if error.errno != errno.EEXIST:
+                raise
+
+    return
+
+def createFileName(directory_path):
+    timestring = time.strftime("%Y%m%d-%H%M%S")
+    filename = directory_path + "vid_" + timestring + ".h264"
+
+    return filename
+
+def startRecording(filename, directory_path, camera):
+    camera.start_recording(filename, format='h264', quality=35)
+    recordForADay(directory_path, camera)
+
+    return
+
+def recordForAnHour(camera):
+    isItNight = obtainLightSensorReading()
+    minute_counter = 0
+    secondsInAnHour= 3600
+    secondsInFifteenMinutes = 900
+    while minute_counter < secondsInAnHour:
+        #Check if it's night time every 15 minutes
+        if ((minute_counter % secondsInFifteenMinutes) == 0):
+            isItNight = obtainLightSensorReading()
+        camera.annotate_text = time.strftime("%H%M%S")
+        camera.wait_recording(1)
+        minute_counter += 1
+
+    return
+
+def recordForADay(directory_path, camera):
+    recordForAnHour(camera)
+    hour_counter = 1
+    hoursInADay = 24
+    while hour_counter < hoursInADay:
+        splitVideoIntoHours(directory_path, camera)
+        recordForAnHour(camera)
+    camera.stop_recording()
+
+    return
+
+def splitVideoIntoHours(directory_path, camera):
+    timestring = time.strftime("%Y%m%d-%H%M%S")
+    filename = directory_path + "vid_" + timestring + ".h264"
+    camera.split_recording(filename)
+
+    return
+
+def isItNightTime(visible_spectrum_val):
+    nightTime = False
+    minDayTimeLux = 100
+    if (visible_spectrum_val < minDayTimeLux):
+        nightTime = True
+        print "It is Night Time! Activating Camera Night Mode."
+    else:
+        print "It is Day Time! Activating Camera Day Mode."
+
+    return nightTime
+
+# Turn on Infrared LED when Night
+def turnLEDOnOrOff(isItNight):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarning(False)
+    GPIO.setup(18, GPIO.OUT)
+    if (isItNight == True):
+        GPIO.output(18, GPIO.HIGH)
+    else:
+        GPIO.output(18, GPIO.LOW)
+    return
+
+def obtainLightSensorReading():
+    # Get I2C bus
+    bus = smbus.SMBus(1)
+
+    # TSL2561 Address = 0x29
+    # Select control register, 0x00, with command register, 0x80
+    #   0x03 is Power ON Mode
+    bus.write_byte_data(0x29, 0x00 | 0x80, 0x03)
+
+    # Select timing register, 0x01, with command register, 0x80
+    #   0x02    Nominal Integration Time = 402ms
+    bus.write_byte_data(0x29, 0x01 | 0x80, 0x02)
+
+    # Sleep for 0.5 seconds
+    time.sleep(0.5)
+
+
+    # Read data back from 0x0C with command register, 0x80, 2 bytes
+    # ch0 LSB, ch0 MSB
+    full_spectrum_data = bus.read_i2c_block_data(0x29, 0x0C | 0x80, 2)
+
+    # Full spectrum = IR + Visible
+
+    # Read data back from 0x0E with command register, 0x80, 2 bytes
+    # ch1 LSB, ch1 MSB
+    infrared_data = bus.read_i2c_block_data(0x29, 0x0E | 0x80, 2)
+
+    # Convert the data
+    full_spectrum_val = full_spectrum_data[1] * 256 + full_spectrum_data[0]
+    infrared_val = infrared_data[1] * 256 + infrared_data[0]
+    visible_spectrum_val = full_spectrum_val - infrared_val
+
+    # Print data to screen
+    print "Full Spectrum Light (IR + Visibile) : %d lux" % full_spectrum_val
+    print "Infrared Value : %d lux" % infrared_val
+    print "Visibile Spectrum Light : %d lux" % visible_spectrum_val
+    isItNight = isItNightTime(visible_spectrum_val)
+    print
+
+    return isItNight
+
+def setSystemToNightMode():
+    turnOnIRLED()
+    return
+
+def setSystemToDayMode():
+    turnOffIRLED()
+    return
+
+#Turn off LED on pin 18
+def turnOffIRLED():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(18, GPIO.OUT)
+    print 'LED off'
+    GPIO.output(18, GPIO.LOW)
+
+#Turn on LED on pin 18
+def turnOnIRLED():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(18, GPIO.OUT)
+    print 'LED on'
+    GPIO.output(18, GPIO.HIGH)
+
+startRaspCamera()
+
+
+
+
+
+
+
+
