40213b2adc40db5978038ea2fe464f6ab86024f9
buckeyebrown <brown.4414@osu.edu>
2017-04-22 21:42:40 -0400

    javascript attempt

--- webserver.js
+++ webserver.js
@@ -1,5 +1,17 @@
 
 function displayVideos(){
+    var dir = "recorded_videos/";
+    var fileExtension = ".mp4";
+    $.ajax({
+        url: dir,
+        success: function (data) {
+            $(data).find("a:contains(" + fileExtension + ")").each(function () {
+                var filename = this.href.replace(window.location, "").replace("http://", "");
+                console.log(filename);
+            });
+        }
+    });
+
     innerHTML = 'hi';
     $(".displayVideoDirs").html(innerHTML);
 }
