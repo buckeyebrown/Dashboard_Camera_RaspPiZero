d4fa94c123bc98f7eceb88efc5b854f57932e957
buckeyebrown <brown.4414@osu.edu>
2017-04-22 20:34:46 -0400

    starting webserver

--- webserver.js
+++ webserver.js
@@ -1,8 +1,1 @@
-var express = require('express');
-var app = express();
-
-
-app.use('/videos', express.static(__dirname + 'index.html'));
-
-app.listen(80).on('error', function(err) { console.trace(err) } );
 
