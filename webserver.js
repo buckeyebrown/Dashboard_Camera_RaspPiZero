8b1ac17e920fe1799bf862a14d95582fd6115945
buckeyebrown <brown.4414@osu.edu>
2017-04-22 22:30:13 -0400

    working on website

--- webserver.js
+++ webserver.js
@@ -7,10 +7,12 @@ function displayVideos(){
         success: function (data) {
             $(data).find("a:contains(" + fileExtension + ")").each(function () {
                 var filename = this.href.replace(window.location, "").replace("http://", "");
-                var something = filename.split('_')[1].split('-');
+                var filetimestamp = filename.split('_')[1].split('-');
+                var yearMonthDay = filetimestamp[0];
+                var hourMinSec = filetimestamp[1].split('.')[0];
                 console.log(filename);
-                console.log(something[0]);
-                console.log(something[1]);
+                console.log(yearMonthDay);
+                console.log(hourMinSec);
             });
         }
     });
